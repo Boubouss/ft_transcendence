21dfa3d2bf0d32333bf92a3f8a73adb2f62c9079
undeadamien <damien.trala@outlook.com>
2025-05-20 14:52:03 +0200

    third player, PlayerInput, basic user input

--- sources/logic/src/game/Paddle.ts
+++ sources/logic/src/game/Paddle.ts
@@ -1,22 +1,27 @@
+import { PlayerInput } from "../type/Type";
+
 export class Paddle {
   private x: number;
   private y: number;
   private h: number;
   private w: number;
-  private dx: number;
-  private dy: number;
+  private step: number;
 
-  constructor(x: number, y: number, h: number = 80, w: number = 10) {
+  constructor(x: number, y: number, h: number = 80, w: number = 10, step = 5) {
     this.x = x;
     this.y = y;
     this.h = h;
     this.w = w;
-    this.dx = 0;
-    this.dy = 0;
+    this.step = step;
   }
   public setPosition(x: number, y: number) {
     this.x = x;
     this.y = y;
+  }
+  public move(input: PlayerInput) {
+    if (input === null) return;
+    if (input === "up") this.y -= this.step;
+    if (input === "down") this.y += this.step;
   }
   public toJson() {
     return { x: this.x, y: this.y, h: this.h, w: this.w };
