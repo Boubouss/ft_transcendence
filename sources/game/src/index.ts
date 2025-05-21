8a3c5528ed13aa1186818e9232c71c2b14c9f126
Boubouss <redwanbsm@gmail.com>
2025-05-21 15:30:59 +0200

    CU-86994w32w - Initialisation Game API

---
+++ sources/game/src/index.ts
@@ -0,0 +1,40 @@
+import * as dotenv from "dotenv";
+import { checkEnv } from "./env";
+import path from "path";
+import fs from "fs";
+import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import { errorHandler } from "./errors/errorHandler";
+import fastifyWebsocket from "@fastify/websocket";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+checkEnv();
+
+const app = fastify({
+	https: {
+		key: fs.readFileSync(path.resolve(__dirname, process.env.HTTPS_KEY)),
+		cert: fs.readFileSync(path.resolve(__dirname, process.env.HTTPS_CERT)),
+	},
+});
+
+app.register(fastifyWebsocket);
+
+app.register(fastifyJwt, {
+	secret: process.env.JWT_KEY,
+});
+
+app.setErrorHandler(errorHandler);
+
+const start = async () => {
+	try {
+		await app.listen({ port: 3000 });
+		console.log("Server is running on https://localhost:3000");
+	} catch (err) {
+		app.log.error(err);
+		console.log(err);
+		process.exit(1);
+	}
+};
+
+start();
