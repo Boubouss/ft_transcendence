46098f6eae60ae76ef9ce59b2a4445bae0d0d110
Enzo Viala <eviala@f2r3s10.clusters.42paris.fr>
2025-06-09 14:57:21 +0200

    nickel modif user rapide

--- sources/user/src/routes/crud.ts
+++ sources/user/src/routes/crud.ts
@@ -21,26 +21,26 @@ const crud: FastifyPluginAsync = async (fastify) => {
 	fastify.get("/user/:id", async (request, reply) => {
 		const { id } = request.params as { id: string };
 
-		reply.send(getUserById(parseInt(id)));
+		reply.send(await getUserById(parseInt(id)));
 	});
 
 	fastify.post("/user", { schema: createSchema }, async (request, reply) => {
 		const user: UserCreate = request.body as UserCreate;
 
-		reply.send(createUser(user));
+		reply.send(await createUser(user));
 	});
 
 	fastify.put("/user/:id", { schema: updateSchema }, async (request, reply) => {
 		const { id } = request.params as { id: string };
 		const user: UserUpdate = request.body as UserUpdate;
 
-		reply.send(updateUser(parseInt(id), user));
+		reply.send(await updateUser(parseInt(id), user));
 	});
 
 	fastify.delete("/user/:id", async (request, reply) => {
 		const { id } = request.params as { id: string };
 
-		reply.send(deleteUser(parseInt(id)));
+		reply.send(await deleteUser(parseInt(id)));
 	});
 };
 
