6fbf216fe9b7ef443e8826084508ba8a0ca81cdf
Boubouss <redwanbsm@gmail.com>
2025-07-24 14:49:03 +0200

    ok

--- sources/front/src/components/Friends/Friends.ts
+++ sources/front/src/components/Friends/Friends.ts
@@ -1,25 +1,49 @@
 import { useEffect, useState } from "#core/framework.ts";
 import { createElement } from "#core/render.ts";
+import { handleSocketFriends } from "#requests/friendsRequest.ts";
+import { getStorage } from "#services/data.ts";
 import type { Friend } from "#types/user.ts";
 
 const Friends = () => {
 	const [friends, setFriends] = useState<Friend[] | null>([]);
+	console.log("test");
 
-	useEffect(() => {
-		const socket = new WebSocket(
-			`${import.meta.env.VITE_LOBBY_WSS}/${user_id}`
-		);
+	const [test, setTest] = useState(false);
 
-		socket.onmessage = (event) => handleSocketFriend(event);
+	const user = getStorage(sessionStorage, "transcendence_user");
+	const token = getStorage(localStorage, "transcendence_conf").token;
 
-		return () => {
-			if (socket.readyState === WebSocket.OPEN) {
-				socket.close();
-			}
-		};
-	}, []);
+	// useEffect(() => {
+	// 	const socket = new WebSocket(
+	// 		`${import.meta.env.VITE_LOBBY_WSS}/${user.id}?token=${token}`
+	// 	);
 
-	return createElement("div", {}, "Friend List ...");
+	// 	socket.onmessage = (message) => handleSocketFriends(message);
+
+	// 	return () => {
+	// 		if (socket.readyState === WebSocket.OPEN) {
+	// 			socket.close();
+	// 		}
+	// 	};
+	// }, []);
+
+	// useEffect(() => {
+	// 	console.log(test);
+	// }, [test]);
+
+	// console.log(test);
+
+	return createElement(
+		"div",
+		{
+			onClick: () => {
+				// console.log(test);
+				setTest(!test);
+				// console.log(test);
+			},
+		},
+		"Friend List ..." + test
+	);
 };
 
 export default Friends;
--- sources/front/src/core/hooks/useState.ts
+++ sources/front/src/core/hooks/useState.ts
@@ -6,17 +6,19 @@ let states: any[] = [];
 export const resetStates = () => {
 	index = 0;
 	states = [];
-}
+};
 
 export function useState<T>(initialValue: T): [T, (toSet: T) => void] {
 	const currentIndex = index;
 
 	if (!states[currentIndex]) {
-		states[currentIndex] = JSON.parse(JSON.stringify(initialValue));
+		states[currentIndex] = initialValue;
 	}
 
-	const setState = (newValue: any) => {
-		states[currentIndex] = JSON.parse(JSON.stringify(newValue));
+	const setState = (newValue: T) => {
+		// console.log("state: ", newValue);
+		states[currentIndex] = newValue;
+		// console.log(states, currentIndex);
 		index = 0;
 		reRender();
 	};
