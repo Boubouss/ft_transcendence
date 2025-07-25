36f4b25f3d1b92571bc31dd39d7aedc90ec707cb
LounisAbdellou <lounisabdellou.dev@gmail.com>
2025-07-25 21:42:47 +0200

    CU-8699y5em9 - Implement reconnect timeout

--- sources/game/src/routes/lobby.ts
+++ sources/game/src/routes/lobby.ts
@@ -6,11 +6,11 @@ import { FastifyPluginAsync } from "fastify";
 import _ from "lodash";
 
 import {
-	createLobby,
-	getLobbyPlayer,
-	handleAction,
-	leaveLobby,
-	whisperData,
+  createLobby,
+  getLobbyPlayer,
+  handleAction,
+  leaveLobby,
+  whisperData,
 } from "#services/lobbyService";
 
 export const sockets: Map<number, WebSocket> = new Map();
@@ -18,68 +18,91 @@ export const lobbies: Lobby[] = [];
 export const players: LobbyPlayer[] = [];
 export const tournaments: Tournament[] = [];
 
+const playerTimeout: Map<number, ReturnType<typeof setTimeout>> = new Map();
+
 export const lobby: FastifyPluginAsync = async (fastify) => {
-	fastify.get(
-		"/lobby/:user_id",
-		{ websocket: true },
-		async (socket, request) => {
-			const { user_id } = request.params as { user_id: string };
-			const player_id = parseInt(user_id);
-			sockets.set(player_id, socket);
+  fastify.get(
+    "/lobby/:user_id",
+    { websocket: true },
+    async (socket, request) => {
+      const { user_id } = request.params as { user_id: string };
+      const player_id = parseInt(user_id);
+      sockets.set(player_id, socket);
 
-			await getLobbyPlayer(
-				user_id,
-				socket,
-				request.headers["sec-websocket-protocol"],
-			);
+      if (_.isEmpty(players.find((p) => p.id === player_id))) {
+        await getLobbyPlayer(
+          user_id,
+          socket,
+          request.headers["sec-websocket-protocol"],
+        );
+      } else {
+        const timeout = playerTimeout.get(player_id);
 
-			socket.on("message", async (message: string) => {
-				const { event, data } = JSON.parse(message);
+        clearTimeout(timeout);
 
-				const player = players.find((p) => p.id === player_id);
-				if (_.isEmpty(player)) {
-					return socket.send(
-						JSON.stringify({
-							event: ClientEvent.ERROR,
-							data: { message: "This player doesn't exist." },
-						}),
-					);
-				}
+        socket.send(
+          JSON.stringify({ event: ClientEvent.LOBBY_LIST, data: lobbies }),
+        );
+      }
 
-				switch (event) {
-					case ServerEvent.CREATE:
-						return createLobby(player, data);
-					case ServerEvent.ACTION:
-						return handleAction(player, data);
-					default:
-						return whisperData(
-							[player.id],
-							JSON.stringify({
-								event: ClientEvent.ERROR,
-								data: { message: "This event doesn't exist." },
-							}),
-						);
-				}
-			});
+      socket.on("message", async (message: string) => {
+        const { event, data } = JSON.parse(message);
 
-			socket.on("close", async () => {
-				const playerIndex = players.findIndex((p) => p.id === player_id);
-				if (playerIndex === -1) return;
+        const player = players.find((p) => p.id === player_id);
+        if (_.isEmpty(player)) {
+          return socket.send(
+            JSON.stringify({
+              event: ClientEvent.ERROR,
+              data: { message: "This player doesn't exist." },
+            }),
+          );
+        }
 
-				const lobby = lobbies.find((l) => l.id === players[playerIndex].id);
-				if (!_.isEmpty(lobby)) {
-					leaveLobby(lobby, players[playerIndex]);
+        switch (event) {
+          case ServerEvent.CREATE:
+            return createLobby(player, data);
+          case ServerEvent.ACTION:
+            return handleAction(player, data);
+          default:
+            return whisperData(
+              [player.id],
+              JSON.stringify({
+                event: ClientEvent.ERROR,
+                data: { message: "This event doesn't exist." },
+              }),
+            );
+        }
+      });
 
-					if (lobby.is_tournament) {
-						handleTournamentLeave(players[playerIndex]);
-					}
-				}
+      socket.on("close", async () => {
+        const timeout = setTimeout(() => {
+          const playerIndex = players.findIndex((p) => p.id === player_id);
+          if (playerIndex === -1) return;
 
-				players.splice(playerIndex, 1);
-				sockets.delete(player_id);
-			});
-		},
-	);
+          const lobby = lobbies.find((lobby) => {
+            const player = lobby.players.find((p) => {
+              return p.id === players[playerIndex].id;
+            });
+
+            return !_.isEmpty(player);
+          });
+
+          if (!_.isEmpty(lobby)) {
+            leaveLobby(lobby, players[playerIndex]);
+
+            if (lobby.is_tournament) {
+              handleTournamentLeave(players[playerIndex]);
+            }
+          }
+
+          players.splice(playerIndex, 1);
+          sockets.delete(player_id);
+        }, 10000);
+
+        playerTimeout.set(player_id, timeout);
+      });
+    },
+  );
 };
 
 export default lobby;
