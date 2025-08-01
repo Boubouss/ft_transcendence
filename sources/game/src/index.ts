15d449667501a61d18f5d4bc36b263b2e020ec9d
Fano435 <rasamimananajean@gmail.com>
2025-08-01 16:14:14 +0200

    In progress

--- sources/game/src/index.ts
+++ sources/game/src/index.ts
@@ -16,20 +16,18 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 checkEnv();
 
 const app = fastify({
-	https: {
-		key: fs.readFileSync(process.env.HTTPS_KEY as string),
-		cert: fs.readFileSync(process.env.HTTPS_CERT as string)
-	}
+    https: {
+        key: fs.readFileSync(process.env.HTTPS_KEY as string),
+        cert: fs.readFileSync(process.env.HTTPS_CERT as string),
+    },
 });
 
-app.register(
-  cors, {
+app.register(cors, {
     origin: "http://localhost:5173",
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     preflightContinue: false,
-  }
-);
+});
 
 app.register(fastifyWebsocket);
 app.register(fastifyJwt, { secret: process.env.JWT_KEY as string });
@@ -41,14 +39,14 @@ app.register(lobby);
 app.setErrorHandler(errorHandler);
 
 const start = async () => {
-	try {
-		await app.listen({ port: 3001 });
-		console.log("Server is running on https://localhost:3001");
-	} catch (err) {
-		app.log.error(err);
-		console.log(err);
-		process.exit(1);
-	}
+    try {
+        await app.listen({ port: 3000, host: "0.0.0.0" });
+        console.log("Server is running on https://localhost:3001");
+    } catch (err) {
+        app.log.error(err);
+        console.log(err);
+        process.exit(1);
+    }
 };
 
 start();
