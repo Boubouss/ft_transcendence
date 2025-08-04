e6618fb2512e9ebeae12458f44cc7054aab324e8
Redwane Bouselham <89694238+Boubouss@users.noreply.github.com>
2025-08-04 16:50:14 +0200

    CU-8699zptwy - API User - FormData (#46)

--- sources/user/src/routes/crud.ts
+++ sources/user/src/routes/crud.ts
@@ -1,5 +1,5 @@
 import { FastifyPluginAsync } from "fastify";
-import { Players, UserCreate, UserUpdate } from "../types/types";
+import { Players, UserCreate, UserData, UserUpdate } from "../types/types";
 import {
 	createSchema,
 	playersSchema,
@@ -12,8 +12,10 @@ import {
 	deleteUser,
 	getPlayers,
 	getUser,
+	uploadAvatar,
 } from "../services/userService";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { ajv } from "..";
 
 const crud: FastifyPluginAsync = async (fastify, opts) => {
 	fastify.addHook("preHandler", authMiddleware);
@@ -32,10 +34,56 @@ const crud: FastifyPluginAsync = async (fastify, opts) => {
 		return createUser(user);
 	});
 
-	fastify.put("/user/:id", { schema: updateSchema }, async (request, reply) => {
-		const { id } = request.params as { id: string };
-		const user: UserUpdate = request.body as UserUpdate;
-		return updateUser(parseInt(id), user);
+	fastify.put("/user/:id", async (request, reply) => {
+		const validate = ajv.compile(updateSchema);
+
+		try {
+			const { id } = request.params as { id: string };
+
+			const data = request.parts();
+			const user: UserData = {};
+
+			for await (const part of data) {
+				if (
+					part.type === "file" &&
+					part.mimetype !== "application/octet-stream"
+				) {
+					const upload = await uploadAvatar(parseInt(id), part);
+					if (!upload) return reply.send({ message: "upload ko" });
+					user.avatar = upload;
+				} else {
+					const value = part.value as string;
+					switch (part.fieldname) {
+						case "name":
+							if (value.length > 0) user.name = value;
+							break;
+						case "email":
+							if (value.length > 0) user.email = value;
+							break;
+						case "password":
+							if (value.length > 0) user.password = value;
+							break;
+						case "isA2F":
+							user.configuration = { is2FA: value === "true" };
+							break;
+						default:
+							break;
+					}
+				}
+			}
+
+			if (validate(user)) {
+				const response = await updateUser(parseInt(id), user);
+				return reply.send({ event: "UPDATE", data: response });
+			}
+
+			return reply.send({
+				event: "ERROR",
+				errors: validate.errors?.shift()?.instancePath,
+			});
+		} catch (error) {
+			reply.send({ error });
+		}
 	});
 
 	fastify.delete("/user/:id", async (request, reply) => {
